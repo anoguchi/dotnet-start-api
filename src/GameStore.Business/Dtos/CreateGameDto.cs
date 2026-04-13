05a2379fa0bee1b6014a3e82aa92147c24232328
Alberto Noguchi <alberto2611@gmail.com>
2026-04-13 18:39:01 -0300

    basic endpoints

---
+++ src/GameStore.Business/Dtos/CreateGameDto.cs
@@ -0,0 +1,8 @@
+﻿namespace GameStore.Business.Dtos;
+
+public record CreateGameDto(
+    string Name,
+    string Genre,
+    decimal Price,
+    DateOnly ReleaseDate
+);
---
+++ src/GameStore.Business/Dtos/GameDto.cs
@@ -0,0 +1,9 @@
+﻿namespace GameStore.Business.Dtos;
+
+public record GameDto(
+    Guid Id,
+    string Name,
+    string Genre,
+    decimal Price,
+    DateOnly ReleaseDate
+);
