4523855c8470bd24d69afea38049f49be54f3c60
Alberto Noguchi <alberto2611@gmail.com>
2026-04-16 16:05:58 -0300

    add: services interface

---
+++ src/GameStore.Business/Interfaces/IGameService.cs
@@ -0,0 +1,10 @@
+﻿using GameStore.Business.Dtos;
+
+namespace GameStore.Business.Interfaces;
+
+public interface IGameService : IDisposable
+{
+    Task AddGameAsync(CreateGameDto newGame);
+    Task UpdateGameAsync(Guid id, UpdateGameDto updateGame);
+    Task DeleteGameAsync(Guid id);
+}
